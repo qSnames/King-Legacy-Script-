375b8ef027e82ce34bcb79eaae733d6363186f16
qSnames <118021849+qSnames@users.noreply.github.com>
2022-11-11 22:24:30 +0100

    Update KingLegacy Script Gui.lua

--- KingLegacy Script Gui.lua
+++ KingLegacy Script Gui.lua
@@ -71,16 +71,16 @@ Tab:AddButton({
     Name = "Key: K CameraPanty:By Miloszurek/qSname",
     Callback = function()
               local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
-local pro = false
+local kol = false
 local witekmadowna = "k"
 
 Mouse.KeyDown:Connect(function(Key)
     if Key == witekmadowna then
-        if pro then
-            pro = false
+        if kol then
+            kol = false
         else
-            pro = true
-            while pro and wait() do
+            kol = true
+            while kol and wait() do
                 game:GetService("Workspace").Camera.CFrame = CFrame.new(778.791687, -51.0170288, 1556.51135, -0.558143377, -0.817138314, 0.144086659, 0, -0.173651844, 0.984807074, 0.829744577, -0.549663544, 0.0969226286)
             end
         end
@@ -89,3 +89,22 @@ end)
       end
 })
 OrionLib:Init()
+
+local Tab = Window:MakeTab({
+    Name = "Lcok",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+       
+})
+Tab:AddButton({
+    Name = "Mouse/U Cant To Off THisd when u click this u must after rejoin",
+    Callback = function()
+            
+                 local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
+RunService.RenderStepped:Connect(function()
+    UserInputService.MouseBehavior = Enum.MouseBehavior.LockCenter
+end)
+
+      end
+})
